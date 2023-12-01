ac633c9a6d060d6da8d3faf83d2f89ec1a92a573
sandra prinson <sandraprinson31@gmail.com>
2023-12-01 10:06:41 +0530

    renovation of site

---
+++ assets/js/main.js
@@ -0,0 +1,80 @@
+jQuery(function ($) {
+    'use strict';
+	
+	// Header Sticky
+	$(window).on('scroll',function() {
+		if ($(this).scrollTop() > 120){  
+			$('.navbar-area').addClass("is-sticky");
+		}
+		else{
+			$('.navbar-area').removeClass("is-sticky");
+		}
+	});
+
+	// Mean Menu
+	jQuery('.mean-menu').meanmenu({
+		meanScreenWidth: "1199"
+	});
+	
+	// Others Option For Responsive JS
+	$(".others-option-for-responsive .dot-menu").on("click", function(){
+		$(".others-option-for-responsive .container .container").toggleClass("active");
+	});
+
+
+
+	// Go to Top
+	$(function(){
+		// Scroll Event
+		$(window).on('scroll', function(){
+			var scrolled = $(window).scrollTop();
+			if (scrolled > 600) $('.go-top').addClass('active');
+			if (scrolled < 600) $('.go-top').removeClass('active');
+		});  
+		// Click Event
+		$('.go-top').on('click', function() {
+			$("html, body").animate({ scrollTop: "0" },  500);
+		});
+	});
+
+
+	
+	
+	// Main News Slides
+	$('.main-news-slides').owlCarousel({
+		loop: true,
+		nav: true,
+		dots: false,
+		autoplayHoverPause: true,
+		autoplay: true,
+		margin: 30,
+		navText: [
+			"<i class='bx bx-chevron-left'></i>",
+			"<i class='bx bx-chevron-right'></i>"
+		],
+		responsive: {
+			0: {
+				items: 1,
+			},
+			576: {
+				items: 1,
+			},
+			768: {
+				items: 2,
+			},
+			1200: {
+				items: 3,
+			}
+		}
+	});
+	
+	
+	
+	// Preloader
+	$(window).on('load', function() {
+		$('.preloader').fadeOut();
+		$('.preloader-area').addClass('preloader-deactivate');
+	});
+
+}(jQuery));
+
